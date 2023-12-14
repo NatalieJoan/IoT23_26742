8316e93b626706b67998c09722e08bbf5f92de73
Natalia Helak <nhelak@edu.cdv.pl>
2023-12-14 17:05:45 +0100

    Added Database models and controllers

---
+++ Lab4/Lab4.DTO/Models/Address.cs
@@ -0,0 +1,10 @@
+namespace Lab1.DTO
+{
+    public class Address
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int NumberOfHome { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
---
+++ Lab4/Lab4.Rest/Database/Entities/AddressEntity.cs
@@ -0,0 +1,10 @@
+namespace Lab1.Rest.Database.Entities
+{
+    public class PersonEntity
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int NumberOfHome { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
